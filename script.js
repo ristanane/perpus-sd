f48a57577467d9f5f6e5da4aa6d961ad40523644
ristanane <ranerangi@gmail.com>
2026-07-03 12:24:33 +0700

    Fix typo in comment for data initialization

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ async function muatDataAwal() {
         masterSiswa = data.siswa || [];
         masterBuku = data.buku || [];
         
-        // Panggil inisialisasi di sini agar data sudh terisi
+        // Panggil inisialisasi di sini agar data sudaaaaah terisi
         inisialisasiSistem(); 
         
         renderTabelPeminjaman(data.log || []);
